29c3a5fe82cf9e0e0d9532e4204ade9f2a127bc3
codepipe <47555726+codepipe@users.noreply.github.com>
2024-09-16 09:37:21 +0530

    Update kalyani.py
    please refer my changes

--- kalyani.py
+++ kalyani.py
@@ -1,3 +1,4 @@
 Welcome, kalyani to learn the git stages and understanding the concepts of the git life cycle
 yes i m also adding the second line of the file
 again one more i m adding
+yes, vishwa i m the everyday using the xlookups and powerquery in my excel sheets
